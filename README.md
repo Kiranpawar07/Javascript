f42afa91cae56d3b3f410542e2872f40bc2a982d
Kiranpawar07 <pawark485@gmail.com>
2022-03-06 19:20:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-833uwp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-833uwp)
---
+++ index.js
@@ -0,0 +1,27 @@
+// var i = 1;
+// sum1 = 0;
+// while (i <= 100) {
+//   sum1 = sum1 + i;
+//   i++;
+// }
+// console.log('Sum of Natural  number :: ' + sum1);
+
+// var i = 1;
+// sum2 = 0;
+// while (i <= 100) {
+//   if (i % 2 == 1) {
+//     sum2 = sum2 + i;
+//     i++;
+//   }
+// }
+// console.log('Sum of Even number:: ' + sum2);
+
+// var i = 1;
+// sum3 = 0;
+// while (i <= 100) {
+//   if (i % 2 == 0) {
+//     sum3 = sum3 + i;
+//     i++;
+//   }
+// }
+// console.log('Sum  of Odd number:: ' + sum3);
